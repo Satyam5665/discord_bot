ad74d5c443bc63433cba8f594568e65b49019fa4
Satyam Singh <96643434+Satyam5665@users.noreply.github.com>
2023-12-29 13:09:37 +0530

    Added the Functionality

--- bot.js
+++ bot.js
@@ -1,4 +1,20 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
+client.on("messageCreate", (message) => {
+  if(message.author.bot) return;
+  message.reply({
+    content: "Hi From Bot",
+  });
+});
+
+client.login(
+  process.env.token
+);
